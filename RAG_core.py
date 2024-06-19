f95ff643e4dff45015f2b000ddb4d948140ad52e
ek775 <eliotkmiec@gmail.com>
2024-06-19 11:19:17 -0500

    vector db pipeline

--- RAG_core.py
+++ RAG_core.py
@@ -3,14 +3,16 @@
 # import libraries & helper scripts
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings
+from llama_index.core import Settings
+
+from pipelines.vector_store import initialize_vector_db
 
 #from evaluation.CRUD import CRUD_metrics
 
 import os
 import pandas as pd
 
-# load api key
+# load sensitive stuffs
 key = None
 with open('./SENSITIVE/ek_llama_index_key.txt', 'r') as f:
     key = f.read().strip()
@@ -22,7 +24,7 @@ Settings.embed_model = OpenAIEmbedding(
     model="text-embedding-3-small", embed_batch_size=100
 )
 
-# initialize objects
-
+# initialize db, query engine, chat engine
+client, vector_store, index = initialize_vector_db(data_dir='./textbook_text_data/')
 
 # **Main Loop** 
---
+++ pipelines/vector_store.py
@@ -0,0 +1,40 @@
+"""Helper Script for initializing the Vector Store"""
+
+# imports
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core.extractors import TitleExtractor
+from llama_index.core.ingestion import IngestionPipeline, IngestionCache
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+
+import qdrant_client
+
+def initialize_vector_db(data_dir: str = './textbook_text_data/'):
+    """
+    Helper function for initializing the Vector Store
+    """
+    # read data
+    reader = SimpleDirectoryReader(input_dir=data_dir)
+    documents = reader.load_data()
+
+    # initialize client and vector store
+    client = qdrant_client.QdrantClient(location=":memory:")
+    vector_store = QdrantVectorStore(client=client, collection_name="glyco_store")
+
+    # pipeline
+    pipeline = IngestionPipeline(
+        transformations=[
+            SentenceSplitter(),
+            TitleExtractor(),
+            OpenAIEmbedding()
+        ],
+        vector_store=vector_store
+    )
+
+    # load the vector db
+    pipeline.run(documents)
+    # create index
+    index = VectorStoreIndex.from_vector_store(vector_store)
+
+    return client, vector_store, index
