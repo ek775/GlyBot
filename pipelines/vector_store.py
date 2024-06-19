ea2ccb4ea8429cf33c72664dec3f70b984212620
ek775 <eliotkmiec@gmail.com>
2024-06-19 12:00:28 -0500

    testing db init

--- pipelines/vector_store.py
+++ pipelines/vector_store.py
@@ -15,10 +15,12 @@ def initialize_vector_db(data_dir: str = './textbook_text_data/'):
     Helper function for initializing the Vector Store
     """
     # read data
+    print("Reading data...")
     reader = SimpleDirectoryReader(input_dir=data_dir)
     documents = reader.load_data()
 
     # initialize client and vector store
+    print("Initializing Vector DB...")
     client = qdrant_client.QdrantClient(location=":memory:")
     vector_store = QdrantVectorStore(client=client, collection_name="glyco_store")
 
@@ -33,8 +35,10 @@ def initialize_vector_db(data_dir: str = './textbook_text_data/'):
     )
 
     # load the vector db
-    pipeline.run(documents)
+    pipeline.run(documents=documents)
     # create index
+    print("Creating index...")
     index = VectorStoreIndex.from_vector_store(vector_store)
+    print("Done!")
 
     return client, vector_store, index
