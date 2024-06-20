e39719b31b04d755fe98b3e21d4a06a71eba81bb
ek775 <eliotkmiec@gmail.com>
2024-06-20 13:20:17 -0500

    eval pipeline draft

--- evaluation/response.py
+++ evaluation/response.py
@@ -23,17 +23,18 @@ class glybot_response_metrics(BaseEvaluator):
     """
     Class for running response evaluation in the main experimentation pipeline.
     """
-    def __init__(self, test_data, query_engine):
+    def __init__(self, curated_q_path, documents, query_engine):
         """
         Initialize the evaluator with the test data and query engine.
         """
-        self.test_data = test_data
+        self.documents = documents
+        self.curated_q_path = curated_q_path
         self.query_engine = query_engine
-        self.llm_metrics = {
+        self.llm_metrics = [
             FaithfulnessEvaluator(),
             RelevancyEvaluator(),
             CorrectnessEvaluator()
-        }
+        ]
         self.ragas_metrics = [
             faithfulness,
             answer_relevancy,
@@ -41,13 +42,60 @@ class glybot_response_metrics(BaseEvaluator):
             context_recall,
             harmfulness
         ]
+
         def transform(self):
             """
             Processes the test data for evaluation.
             """
-            pass
-        def evaluate(self):
+            # generate synthetic test set
+            docs = self.documents
+            generator = TestsetGenerator.from_llama_index()
+            synthetic_test_set = generator.generate_with_llamaindex_docs(
+                documents = docs,
+                test_size=20,
+                distributions={
+                    simple: 0.5, 
+                    reasoning: 0.25, 
+                    multi_context: 0.25}
+                )
+            synthetic_dict = synthetic_test_set.to_dataset().to_dict()
+            self.synthetic_dict = synthetic_dict
+
+            # load curated test set
+            curated_test_set = pd.read_csv(self.curated_q_path)
+            curated_dict = {"question":curated_test_set[],
+                            "ground_truth":curated_test_set[]}
+
+            self.curated_dict = None
+
+        def response_evaluation(self):
             """
             Runs the evaluation pipeline.
             """
-            pass
+            ragas_synth = evaluate(
+                query_engine = self.query_engine,
+                metrics = self.ragas_metrics
+                dataset = self.synthetic_dict
+            )
+            llm_synth = evaluate(
+                query_engine = self.query_engine,
+                metrics = self.llm_metrics
+                dataset = self.synthetic_dict
+            )
+            ragas_curated = evaluate(
+                query_engine = self.query_engine,
+                metrics = self.ragas_metrics
+                dataset = self.curated_dict
+            )
+            llm_curated = evaluate(
+                query_engine = self.query_engine,
+                metrics = self.llm_metrics
+                dataset = self.curated_dict
+            )
+            results = [("ragas_synth",ragas_synth), 
+                       ("llm_synth",llm_synth), 
+                       ("ragas_curated",ragas_curated), 
+                       ("llm_curated",llm_curated)]
+            for data in results:
+                df = data[1].to_pandas()
+                df.to_csv(f"./response_evaluation/{data[0]}.csv")
